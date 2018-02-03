2dcc6ee98002a79f885e533478f706ac20f872fd
karthik_b150310cs <saikarthik1211@gmail.com>
2018-02-03 14:54:27 +0530

    included testcases

--- scripts/script_para.sh
+++ scripts/script_para.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Starting Script file \n\n" > "output_para.txt"
-for FILE in *.graph
+for FILE in 'ls *.graph | sort -k 5n'
 do
-		echo "Running on $FILE \n\n" >> "output_para.txt"
-        timeout 30 ./prog < "$FILE" >> "output_para.txt"
+	echo "Running on $FILE \n\n" >> "output_para.txt"
+        timeout 20m ./prog < "$FILE" >> "output_para.txt"
         echo "\n Time Limit Exceeded on $FILE \n\n" >> "output_para.txt"
 done
